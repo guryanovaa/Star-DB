0ded85821da7056082dee934f2e0801cd198dc92
“Alina” <guryanova27@bk.ru>
2019-07-22 21:16:50 +0300

    catch-error

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 
 import './person-details.css';
-import SwapiService from '../../services/swapi-service';
-
+import SwapiService from "../../services/swapi-service";
+import ErrorButton from "../error-button/error-button";
 
 export default class PersonDetails extends Component {
-  
+
   swapiService = new SwapiService();
 
   state = {
     person: null
-  }
+  };
 
   componentDidMount() {
     this.updatePerson();
@@ -27,6 +27,7 @@ export default class PersonDetails extends Component {
     if (!personId) {
       return;
     }
+
     this.swapiService
       .getPerson(personId)
       .then((person) => {
@@ -34,20 +35,21 @@ export default class PersonDetails extends Component {
       });
   }
 
-
   render() {
 
-    if (!this.state.person) {
-       return <span>Salect a person from a list</span>
+    const { person } = this.state;
+    if (!person) {
+      return <span>Select a person from a list</span>;
     }
 
-    const { id, name, gender, brithYear, eyeColor}
-    = this.state.person;
+    const { id, name, gender,
+              birthYear, eyeColor } = person;
 
     return (
       <div className="person-details card">
         <img className="person-image"
-          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} />
+          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
+          alt="character"/>
 
         <div className="card-body">
           <h4>{name}</h4>
@@ -58,13 +60,14 @@ export default class PersonDetails extends Component {
             </li>
             <li className="list-group-item">
               <span className="term">Birth Year</span>
-              <span>{brithYear}</span>
+              <span>{birthYear}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Eye Color</span>
               <span>{eyeColor}</span>
             </li>
           </ul>
+          <ErrorButton />
         </div>
       </div>
     )
